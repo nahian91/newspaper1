4851c4f090bc0fbbeec6bf44316597ab5a11d4f6
nahian28 <nahiansylhet@gmail.com>
2023-08-23 04:18:53 +0600

    Updates

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+ 
+// category start
+
+let topic1=document.querySelector("#topic1");
+let topic2=document.querySelector("#topic2");
+let topic3=document.querySelector("#topic3");
+let topic4=document.querySelector("#topict4");
+let topic5=document.querySelector("#topic5");
+
+
+
+let btn1=document.querySelector("#btn1");
+let btn2=document.querySelector("#btn2");
+let btn3=document.querySelector("#btn3");
+let btn4=document.querySelector("#btn4");
+let btn5=document.querySelector("#btn5");
+
+
+
+function all(){
+    topic1.style.display ="block";
+    topic2.style.display ="none";
+    topic2.style.display ="none";
+    topic2.style.display ="none";
+    topic2.style.display ="none";
+    topic2.style.display ="none";
+
+  
+}
+function click1(){
+    topic1.style.display ="none";
+    topic2.style.display ="block";
+    topic2.style.display ="none";
+    topic2.style.display ="none";
+    topic2.style.display ="none";
+    topic2.style.display ="none";
+
+  
+}
